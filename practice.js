3339c918b4b99af1aa0dd98397ed6d5d78525345
JaneSmith125 <jane.smith@google.com>
2024-12-11 19:41:05 +0200

    Leetcode

--- practice.js
+++ practice.js
@@ -134,3 +134,57 @@
 //     return dummy.next;
 // };
 // console.log(mergeTwoLists(list1, list2));
+
+
+// 26. Remove Duplicates from Sorted Array
+
+// Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place such that each unique element appears only once. The relative order of the elements should be kept the same. Then return the number of unique elements in nums.
+//
+// Consider the number of unique elements of nums to be k, to get accepted, you need to do the following things:
+//
+// Change the array nums such that the first k elements of nums contain the unique elements in the order they were present in nums initially. The remaining elements of nums are not important as well as the size of nums.
+// Return k.
+
+/*
+ * @param {number[]} nums
+ * @return {number}
+ */
+// var removeDuplicates = function(nums) {
+//     let k = 1
+//     for( let i=0; i<nums.length-1; i++) {
+//         if(nums[i]!==nums[i+1]){
+//             nums[k] = nums[i+1]
+//             k++
+//         }
+//     }
+//     return k
+// };
+
+// 27. Remove Element
+
+/*
+Given an integer array nums and an integer val, remove all occurrences of val in nums in-place. The order of the elements may be changed. Then return the number of elements in nums which are not equal to val.
+
+Consider the number of elements in nums which are not equal to val be k, to get accepted, you need to do the following things:
+
+Change the array nums such that the first k elements of nums contain the elements which are not equal to val. The remaining elements of nums are not important as well as the size of nums.
+Return k.
+*/
+
+/*
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+
+var removeElement = function (nums, val) {
+    let k = 0
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] !== val) {
+            nums[k] = nums[i];
+            k++;
+        }
+    }
+    return k;
+};
+
